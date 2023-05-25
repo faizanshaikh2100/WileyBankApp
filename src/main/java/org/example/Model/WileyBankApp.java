3c448cd2d8c2591b2ba000dc9edbd4a214fd1891
unknown <faizanshaikh2100@gmail.com>
2023-05-25 14:56:46 +0530

    fixed some bugs

--- src/main/java/org/example/Model/WileyBankApp.java
+++ src/main/java/org/example/Model/WileyBankApp.java
@@ -3,13 +3,16 @@ package org.example.Model;
 import org.example.Controller.CustomerController;
 import org.example.View.Customerview;
 import org.example.functionalities.Login;
+import org.example.functionalities.SignUp;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class WileyBankApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        try {
             while(true) {
 
                 System.out.println();
@@ -26,7 +29,12 @@ public class WileyBankApp {
                     case 1:
                         break;
                     case 2:
-                        Login.main(args);
+                        System.out.println("    1. Login ");
+                        System.out.println("    2. Register ");
+                        int opt2 = sc.nextInt();
+                        if(opt2==1) Login.main(args);
+                        else if(opt2==2) SignUp.main(args);
+                        else System.out.println("Wrong choice");
                         break;
                     default:
                         System.out.println("Wrong choice");
@@ -34,7 +42,9 @@ public class WileyBankApp {
                 }
 
             }
-
+        }catch (InputMismatchException e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
